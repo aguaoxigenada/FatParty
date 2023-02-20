1a9f6ce5f24f401c85482098d6af319d043a2010
aguaoxigenada <aguaoxigenada@users.noreply.github.com>
2023-02-19 21:56:35 -0500

    Prueba Nivel

--- Source/FatParty/Components/Grabber.cpp
+++ Source/FatParty/Components/Grabber.cpp
@@ -128,8 +128,8 @@ bool UGrabber::GetGrabbableInReach(FHitResult &OutHitResult) const
 	FVector End = Start + GetForwardVector() * MaxGrabDistance;
 	
 	// Para ver las lineas:
-	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
-	DrawDebugSphere(GetWorld(), End, 10, 10, FColor::Blue, false, 5);
+	//DrawDebugLine(GetWorld(), Start, End, FColor::Red);
+	//DrawDebugSphere(GetWorld(), End, 10, 10, FColor::Blue, false, 5);
 
 	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
 	return GetWorld()->SweepSingleByChannel(
